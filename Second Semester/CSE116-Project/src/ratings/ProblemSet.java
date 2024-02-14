685bc1d7c2eb5cf055f4f80bd64e0a8178c04239
Colin Newcomb <cnewc1115@gmail.com>
2024-02-14 12:13:11 -0500

    send it

--- Second Semester/CSE116-Project/src/ratings/ProblemSet.java
+++ Second Semester/CSE116-Project/src/ratings/ProblemSet.java
@@ -3,6 +3,8 @@ package ratings;
 import java.nio.channels.InterruptibleChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class ProblemSet {
 
@@ -36,11 +38,18 @@ public class ProblemSet {
     // 57 returns 12
     // -36 returns 9
     public static int sumOfDigits(int number) {
-        int num=number;
-        
+        int num=0;
+        ArrayList<Integer> ar1=new ArrayList<Integer>();
+        while (number>0){
+            ar1.add(number%10);
+            number/=10;
+        }
+        for (Integer i : ar1){
+            num+=i;
+        }
         return num;
     }
-}
+
     // TODO: Write a public static method named bestKey that takes a HashMap of String to Integer
     // as a parameter and returns a key mapping to the largest Integer. Ties can be broken arbitrarily.
     //If the HashMap is empty, return the empty String
@@ -49,4 +58,19 @@ public class ProblemSet {
     // {"CSE": 100, "MTH": 90, "MGT": 10} returns "CSE"
     // {"cat": 5, "dog": 5, "fox": 4} can return either "cat" or "dog"
     // {} returns ""
-   
+
+    public static String bestKey(HashMap<String, Integer> hash){
+        if (hash.size() == 0){
+            return "";
+        }
+        String bestkey="";
+        int value=0;
+        for (Map.Entry<String, Integer> entry : hash.entrySet()){
+            if (entry.getValue() > value){
+                value = entry.getValue();
+                bestkey= entry.getKey();
+            }
+        }
+        return bestkey;
+    }
+}
--- Second Semester/CSE116-Project/src/tests/TestProblemSet.java
+++ Second Semester/CSE116-Project/src/tests/TestProblemSet.java
@@ -7,17 +7,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class TestProblemSet {
-
     // TODO: Write testing for all 3 methods of the ratings.ProblemSet class
-
-
-public static void main(String[] args) {
-    ArrayList<Double> arr1=new ArrayList<>();
-    arr1.add(1.0);
-    arr1.add(2.0);
-    arr1.add(3.0);
+    @Test
+    public void TestArrayPositive(){
+        //assertEquals(ProblemSet.average()==2.0);
+    }
 }
-}
+
+    // as a parameter and returns a key mapping to the largest Integer. Ties can be broken arbitrarily.
+    //If the HashMap is empty, return the empty String
+    //
+    // Examples
+    // {"CSE": 100, "MTH": 90, "MGT": 10} returns "CSE"
+    // {"cat": 5, "dog": 5, "fox": 4} can return either "cat" or "dog"
+    // {} returns ""
+
+    //returns the sum of the digits of the input as an int
+    //
+    // Examples
+    // 123 returns 6
+    // 57 returns 12
+    // -36 returns 9
+    
+    //If the ArrayList is empty, return 0.0
+    //
+    // Examples
+    // [1.0,2.0,3.0] returns 2.0
+    // [-5.0,5.0] returns 0.0
+    // [6.5,6.5,8.5,8.5] returns 7.5
+    // [] returns 0.0
