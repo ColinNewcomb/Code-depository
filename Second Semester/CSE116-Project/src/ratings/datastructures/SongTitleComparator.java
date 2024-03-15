fe4493c350f11ed26352c20013ff9f04be8f60d4
Colin Newcomb <cnewc1115@gmail.com>
2024-03-14 23:01:00 -0400

    "main"

--- Second Semester/CSE116-Project/src/ratings/datastructures/SongTitleComparator.java
+++ Second Semester/CSE116-Project/src/ratings/datastructures/SongTitleComparator.java
@@ -1,12 +1,10 @@
 package ratings.datastructures;
 
 import ratings.Song;
-import ratings.Ratable;
+
 
 public class SongTitleComparator extends Comparator<Song>{
-    
-    @Override
-    public boolean compare(Song song1,Song song2){
-        return (song1.getTitle().compareToIgnoreCase(song2.getTitle()))<0;
+    public boolean compare(Song a,Song b){
+        return ((a.getTitle().compareToIgnoreCase(b.getTitle()))<0);
     }
 }
